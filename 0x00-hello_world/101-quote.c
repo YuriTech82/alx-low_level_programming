b87fe0e17ca8220cd891ed6cb817afc39a3bbb74
YuriTech82 <shinjungtee@gmail.com>
2022-12-08 13:07:47 -0800

    Task 4 to 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,14 @@
-#include <stdio.h>                              
-#include <unistd.h>                                                                             
-/**                                              
- *main - Entry point                             
- *                                               
+#include <stdio.h>
+#include<unistd.h>
+/**
+ *main - Entry point
+ *
  *Return: Always 1 (Success)
  */
 int main(void)
 {
-        char *mgs = "and that piece of art is useful\" -Dora Korpar, 2015-10-19\n";
-        write(STDOUT_FILENO, mgs,59);
-        return   (1);                                                                                                                  
- }
+	char *mgs = "and that piece of art is useful\" -Dora Korpar, 20 15-10-19\n";
+
+	write(STDOUT_FILENO, mgs, 59);
+	return (1);
+}
