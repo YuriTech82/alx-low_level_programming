986fe93d9cc6cc2274ab5330e587bf7a0610f509
YuriTech82 <shinjungtee@gmail.com>
2022-12-13 12:45:39 -0800

    Task 6 to 11

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -13,11 +13,11 @@ void jack_bauer(void)
 	for (min = 0; min <= 59; ++min)
 	{
 	_putchar((hr / 10) + 48);
-        _putchar((hr % 10) + 48);
-        _putchar(':');
-        _putchar((min / 10) + 48);
-        _putchar((min % 10) + 48);
-        _putchar('\n');
+	_putchar((hr % 10) + 48);
+	_putchar(':');
+	_putchar((min / 10) + 48);
+	_putchar((min % 10) + 48);
+	_putchar('\n');
 	}
 	}
 }
