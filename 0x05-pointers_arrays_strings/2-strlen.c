4535578948c7f5f4257fe67d6256e9eb9145c23a
YuriTech82 <shinjungtee@gmail.com>
2022-12-19 12:08:16 -0800

    Task 2

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -3,7 +3,7 @@
  * _strlen -return the length of a string
  * @s: char to check
  * Description: this will return the length of a string
- * Return success
+ * Return: 0 is success
  */
 int _strlen(char *s)
 {
