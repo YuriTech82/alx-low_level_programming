d62eab2a09127bd2e7887cdb3079bc2f6f500aea
YuriTech82 <shinjungtee@gmail.com>
2023-03-16 21:03:03 +0300

    All Tasks

---
+++ 0x17-doubly_linked_lists/102-result.py
@@ -0,0 +1,11 @@
+largest_palindrome = 0
+for i in range(100, 1000):
+    for j in range(100, 1000):
+        product = i * j
+        if str(product) == str(product)[::-1]:
+            if product > largest_palindrome:
+                largest_palindrome = product
+
+
+with open("102-result", "w") as file:
+    file.write(str(largest_palindrome))
